95a299d5c3328d365d588705e38c573a76b0d561
Damian Figiński <figo96it@gmail.com>
2022-07-03 21:38:41 +0200

    Initialize new app ForFit

---
+++ src/main/java/com/app/forfit/ForFitApplication.java
@@ -0,0 +1,13 @@
+package com.app.forfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForFitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForFitApplication.class, args);
+    }
+
+}
